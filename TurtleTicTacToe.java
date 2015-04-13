b023aa309676705d985242f80b7977467ac93b23
Daniel Yount <yountdaniel@gmail.com>
2015-04-13 13:39:07 -0400

    Update TurtleTicTacToe.java

--- TurtleTicTacToe.java
+++ TurtleTicTacToe.java
@@ -9,13 +9,12 @@ public class TurtleTicTacToe {
   
   public static void main(String[] args) { 
     
+    // Initializing the world
     World w = new World();
+    // Creating tom turtle to draw board
     Turtle tom = new Turtle(w);
+    // Calling drawBoard method located in Turtle.java
     tom.drawBoard();
-  
-  
-  
+
   }
-  
- 
 }
