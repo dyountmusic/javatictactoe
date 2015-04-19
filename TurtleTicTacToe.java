436fb7c51a9b6eaa33a6e5ba56f045139d7230e8
dyountmusic <yountdaniel@gmail.com>
2015-04-18 23:40:11 -0400

    Ignore .java~
    Ignore all .java~ files

--- TurtleTicTacToe.java
+++ TurtleTicTacToe.java
@@ -15,21 +15,23 @@ public class TurtleTicTacToe {
     Turtle tom = new Turtle(w);
     // Draw the game board
     tom.drawBoard();
-    // Flip a coin to see who goes first
-    boolean userTurn = coinFlip();
+    tom.show();
+    tom.setPenColor(Color.black);
+    tom.penDown();
     
-    // Create variables to store the gameplay
-    boolean winner = false;
-    boolean tie = false;
     
+    // objects to recieve user input
     Scanner scanner = new Scanner(System.in);
     int myInt = 0;
     
-    // Keep track of whose turn it is
-    // X is always the Player
-    // O is always Jarvis
-    char whoseTurn = 'X';
-  
+    // filling the coords array
+    fillCoords(w);
+    
+    // Flip a coin to see who goes first
+    boolean userTurn = coinFlip();
+    boolean winner = false;
+    boolean tie = false;
+    
     while (winner == false && tie == false)
     {
       int i = 0;
@@ -39,86 +41,96 @@ public class TurtleTicTacToe {
         // THIS IS THE USER'S TURN LOGIC
         if (userTurn)
         {
-          // Get User Input
-          System.out.print("Your turn... pick between spots, ");
+          
+          System.out.print("Your turn... pick between spots: ");
           if(movesArray[0][0] != 'x'){
-          System.out.print("1");
+            System.out.print("1, ");
           }
           if(movesArray[0][1] != 'x'){
-          System.out.print(", 2");
+            System.out.print("2,");
           }
           if(movesArray[0][2] != 'x'){
-          System.out.print(", 3");
+            System.out.print(" 3");
           }
           if(movesArray[1][0] != 'x'){
-          System.out.print(", 4");
+            System.out.print(", 4");
           }
           if(movesArray[1][1] != 'x'){
-          System.out.print(", 5");
+            System.out.print(", 5");
           }
           if(movesArray[1][2] != 'x'){
-          System.out.print(", 6");
+            System.out.print(", 6");
           }
           if(movesArray[2][0] != 'x'){
-          System.out.print(", 7");
+            System.out.print(", 7");
           }
           if(movesArray[2][1] != 'x'){
-          System.out.print(", 8");
+            System.out.print(", 8");
           }
           if(movesArray[2][2] != 'x'){
-          System.out.print(", 9");
+            System.out.print(", 9");
           }
+          
           // Get the user's number
           myInt = scanner.nextInt();
           switch (myInt)  {
-          case 1:  movesArray[0][0] = 'x';
-          tom.penUp();
-          tom.moveTo(coordsArray[0][0], coordsArray[0][1]);
-          tom.drawHexagon();
-          break;
-          case 2: movesArray[0][1] = 'x';
-          tom.penUp();
-          tom.moveTo(coordsArray[1][0], coordsArray[1][1]);
-          tom.drawHexagon();
-          break;
-          case 3: movesArray[0][2] = 'x';
-          tom.penUp();
-          tom.moveTo(coordsArray[2][0], coordsArray[2][1]);
-          tom.drawHexagon();
-          break;
-          case 4: movesArray[1][0] = 'x';
-          tom.penUp();
-          tom.moveTo(coordsArray[3][0], coordsArray[3][1]);
-          tom.drawHexagon();
-          break;
-          case 5: movesArray[1][1] = 'x';
-          tom.penUp();
-          tom.moveTo(coordsArray[4][0], coordsArray[4][1]);
-          tom.drawHexagon();
-          break;
-          case 6: movesArray[1][2] = 'x';
-          tom.penUp();
-          tom.moveTo(coordsArray[5][0], coordsArray[5][1]);
-          tom.drawHexagon();
-          break;
-          case 7: movesArray[2][0] = 'x';
-          tom.penUp();
-          tom.moveTo(coordsArray[6][0], coordsArray[6][1]);
-          tom.drawHexagon();
-          break;
-          case 8: movesArray[2][1] = 'x';
-          tom.penUp();
-          tom.moveTo(coordsArray[7][0], coordsArray[7][1]);
-          tom.drawHexagon();
-          break;
-          case 9: movesArray[2][2] = 'x';
-          tom.penUp();
-          tom.moveTo(coordsArray[8][0], coordsArray[8][1]);
-          tom.drawHexagon();
-          break;
-          
+            case 1: movesArray[0][0] = 'x';
+            tom.penUp();
+            tom.moveTo(coordsArray[0][0], coordsArray[0][1]);
+            tom.penDown();
+            tom.drawHexagon();
+            break;
+            case 2: movesArray[0][1] = 'x';
+            tom.penUp();
+            tom.moveTo(coordsArray[1][0], coordsArray[1][1]);
+            tom.penDown();
+            tom.drawHexagon();
+            break;
+            case 3: movesArray[0][2] = 'x';
+            tom.penUp();
+            tom.moveTo(coordsArray[2][0], coordsArray[2][1]);
+            tom.penDown();
+            tom.drawHexagon();
+            break;
+            case 4: movesArray[1][0] = 'x';
+            tom.penUp();
+            tom.moveTo(coordsArray[3][0], coordsArray[3][1]);
+            tom.penDown();
+            tom.drawHexagon();
+            break;
+            case 5: movesArray[1][1] = 'x';
+            tom.penUp();
+            tom.moveTo(coordsArray[4][0], coordsArray[4][1]);
+            tom.penDown();
+            tom.drawHexagon();
+            break;
+            case 6: movesArray[1][2] = 'x';
+            tom.penUp();
+            tom.moveTo(coordsArray[5][0], coordsArray[5][1]);
+            tom.penDown();
+            tom.drawHexagon();
+            break;
+            case 7: movesArray[2][0] = 'x';
+            tom.penUp();
+            tom.moveTo(coordsArray[6][0], coordsArray[6][1]);
+            tom.penDown();
+            tom.drawHexagon();
+            break;
+            case 8: movesArray[2][1] = 'x';
+            tom.penUp();
+            tom.moveTo(coordsArray[7][0], coordsArray[7][1]);
+            tom.penDown();
+            tom.drawHexagon();
+            break;
+            case 9: movesArray[2][2] = 'x';
+            tom.penUp();
+            tom.moveTo(coordsArray[8][0], coordsArray[8][1]);
+            tom.penDown();
+            tom.drawHexagon();
+            break;
+            
           }
-        
+          
           //  Place it into the coordsArra
           userTurn = false;
           
@@ -137,6 +149,20 @@ public class TurtleTicTacToe {
           // Get Computer Turn
           System.out.println("Jarvis is thinking...");
           Thread.sleep(500);
+          
+          // Begin Jarvis Logic
+          
+          // Get a number between 1-9
+          
+          int jarvisNumber = (int)(Math.random()*10);
+          
+          if (jarvisNumber > 9 || jarvisNumber < 1)
+          {
+            jarvisNumber = (int)(Math.random()*10);
+          }
+          
+          System.out.println("Jarvis has chosen space " + jarvisNumber);
+          
           userTurn = true;
           i++;
         }
@@ -145,10 +171,6 @@ public class TurtleTicTacToe {
     }
   }
   
-  // Method to flip a coin to determine who goes first
-  // Method returns a true or false state
-  // true indicates it is the user's turn
-  // false indicates it is Jarvis' turn
   public static boolean coinFlip()
   {
     if(Math.random()<.5)
@@ -183,5 +205,51 @@ public class TurtleTicTacToe {
       Thread.sleep(30);
     }
   }
-
+  
+  public static void fillCoords(World w)
+  {
+    
+    // getting the mathematical measurements of the divided world
+    int height = (int) (w.getHeight()/3);
+    int heightDoubled = (int) (w.getHeight()/3 + w.getHeight()/3);
+    int width = (int) (w.getWidth()/3);
+    int widthDoubled = (int) (w.getWidth()/3 + w.getWidth()/3);
+    
+    // Space 1
+    coordsArray[0][0] = 0;
+    coordsArray[0][1] = 0;
+    
+    // Space 2
+    coordsArray[1][0] = width;
+    coordsArray[1][1] = 0;
+    
+    // Space 3
+    coordsArray[2][0] = widthDoubled;
+    coordsArray[2][1] = 0;
+    
+    // Space 4
+    coordsArray[3][0] = 0;
+    coordsArray[3][1] = height;
+    
+    // Space 5
+    coordsArray[4][0] = width;
+    coordsArray[4][1] = height;
+    
+    // Space 6
+    coordsArray[5][0] = widthDoubled;
+    coordsArray[5][1] = height;
+    
+    // Space 7
+    coordsArray[6][0] = 0;
+    coordsArray[6][1] = heightDoubled;
+    
+    // Space 8
+    coordsArray[7][0] = width;
+    coordsArray[7][1] = heightDoubled;
+    
+    // Space 9
+    coordsArray[8][0] = widthDoubled;
+    coordsArray[8][1] = heightDoubled;
+  }
+  
 }
